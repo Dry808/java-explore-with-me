82fd5b958bc9485752a578500cffc73a3e81ae68
Сухроб Мавлянов <suhrobmavlyanov@yandex.ru>
2025-03-10 22:59:55 +0300

    модули и сервис статистики

---
+++ stats/stats-service/src/main/java/ru/practicum/stats/mapper/EndpointHitMapper.java
@@ -0,0 +1,16 @@
+package ru.practicum.stats.mapper;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.stats.dto.EndpointHitDto;
+import ru.practicum.stats.model.EndpointHit;
+
+@Component
+public class EndpointHitMapper {
+    public EndpointHit toEndpointHit(EndpointHitDto hit) {
+        return new EndpointHit(hit.getId(), hit.getApp(), hit.getUri(), hit.getIp(), hit.getTimestamp());
+    }
+
+    public EndpointHitDto toEndpointHitDto(EndpointHit hit) {
+        return new EndpointHitDto(hit.getId(), hit.getApp(), hit.getUri(), hit.getIp(), hit.getTimestamp());
+    }
+}
